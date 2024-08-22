376fb88c29fddcfe5760bbd8a946d2dccd8971b2
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-22 14:53:52 +0530

    ADDED: login cred validatation and routing to dashboard after succesful login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpService } from 'src/app/service/http/http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,19 +13,25 @@ export class LoginComponent {
     username: new FormControl(''),
     password: new FormControl(''),
   });
-  loginCredential: any ;
 
-  constructor ( private httpService: HttpService) {}
+  constructor ( private httpService: HttpService, private router: Router) {}
 
   
 
   submit() {
     if (this.form.valid) {
       console.log(this.form.value);
+
+      // getting all user login credentials
       this.httpService.getLogin().subscribe({
         next: (res:any) => {
-          console.log(res);
-          this.loginCredential = res;
+
+          // iterating over all user's login credentials to validate the input credential
+          for(let item of res){
+            if(item.username == this.form.value.username && item.password == this.form.value.password) {
+              this.router.navigate(['./dashboard']);
+            }
+          }
         }
       })
     }
