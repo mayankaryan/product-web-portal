f8081c47df0f48e71af97bb687663928195edd3c
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-23 02:38:28 +0530

    BUG: resolve filter function to give related output with price, rating and delivery date

--- src/app/pipe/filter/filter.pipe.ts
+++ src/app/pipe/filter/filter.pipe.ts
@@ -9,39 +9,41 @@ export class FilterPipe implements PipeTransform {
     // console.log('pipe' + price);
     if( !price && !rating && !delivery) return value;
 
+    //------- filter login based on Price
     if(price === 'highTolow'){
       value.sort((prod1: any, prod2: any) => {
         return prod2.price - prod1.price;
       })
     }
-    else {
+    else if(price == 'lowTohigh'){
       value.sort((prod1: any, prod2: any) => {
         return prod1.price - prod2.price;
       })
     }
 
+    //------- filter login based on Rating
     if(rating == 'highTolow') {
       value.sort((prod1: any, prod2: any) => {
         return prod2.rating - prod1.rating;
       })
     }
-    else {
+    else if(rating == 'lowTohigh') {
       value.sort((prod1: any, prod2: any) => {
         return prod1.rating - prod2.rating;
       })
     }
 
+    //------- filter login based on Delivery day
     if(delivery == 'latestOrder') {
       value.sort((prod1: any, prod2: any) => {
         return prod2.delivery_days - prod1.delivery_days;
       })
     }
-    else {
+    else if(delivery == 'deliverySoon') {
       value.sort((prod1: any, prod2: any) => {
         return prod1.delivery_days - prod2.delivery_days;
       })
     } 
-    
     
     return value;
   }
