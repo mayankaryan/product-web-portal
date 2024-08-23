61a2fa3433eaed1913d52632bfba7ade5b9be320
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-23 12:40:43 +0530

    FUNC: implemented adding of review to server throug dialog box

--- src/app/component/product/product/product.component.ts
+++ src/app/component/product/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { HttpService } from 'src/app/service/http/http.service';
 
 @Component({
   selector: 'app-product',
@@ -7,13 +8,21 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  review:string = "";
+  id: number = 0;
+
   constructor(
+    private httpService: HttpService,
     public dialogRef: MatDialogRef<ProductComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   onClose(): void {
     this.dialogRef.close();
+  }
+
+  addReview(data_id: any) {
+    this.httpService.postReview(data_id,this.review);
   }
 }
 
--- src/app/service/http/http.service.ts
+++ src/app/service/http/http.service.ts
@@ -15,4 +15,22 @@ export class HttpService {
   getProductDetails(): any {
     return this.httpClient.get('http://localhost:3000/Product');
   }
+
+  postReview(id:any,review:string): any {
+    // this.httpClient.post(`http://localhost:3000/Product[${id}]`, review)
+
+    this.httpClient.get(`http://localhost:3000/Product/${id}`).subscribe((product: any) => {
+        // Add the new review to the reviews array
+        product.reviews.push(review);
+
+        // Update the product with the new review
+        this.httpClient.put(`http://localhost:3000/Product/${id}`, product).subscribe(response => {
+            console.log('Review added successfully', response);
+        }, error => {
+            console.error('Error adding review', error);
+        });
+    }, error => {
+        console.error('Error fetching product', error);
+    });
+  }
 }
